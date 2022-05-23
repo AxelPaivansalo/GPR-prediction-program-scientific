ae5e7b2a168f244d2bb996e4fc84f308fb9e9aa5
paivana11 <jan.paivansalo@aalto.fi>
2022-05-23 14:45:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,247 @@
+[![foamwood-logo](readme_images/foamwood_logo.png)](https://foamwoodproject.com/)
+
+# GPR prediction program (scientific)
+
+A program for predicting rheological and strength properties of FoamWood utilizing Gaussian Process Regression (GPR).
+
+<figure>
+  <img
+  src="readme_images/foamwood_product_image.jpg"
+  alt="foamwood-product-image">
+  <figcaption>FoamWood can be used as a material in various products.</figcaption>
+</figure>
+
+<figure>
+  <img
+  src="readme_images/loss_modulus_yield_stress.png"
+  alt="2D-image">
+  <figcaption>Image of a 2D interpolation plot that the program outputs.</figcaption>
+</figure>
+
+<figure>
+  <img
+  src="readme_images/youngs_modulus_density_storacell.png"
+  alt="3D-image">
+  <figcaption>Image of a 3D interpolation plot that the program outputs.</figcaption>
+</figure>
+
+# Requirements
+
+1. Python
+    - I used version 3.9.12 but others will probably work as well
+
+2. All the packages listed in the beginning of
+    - main_program.py
+    - additional_methods.py
+    - data_methods.py
+
+The packages can be installed with pip for example.
+
+# Example 1 (simple)
+
+1. Navigate to the folder that you just downloaded from GitHub with the command
+```
+cd FILL-THIS-IN-YOURSELF\GPR-prediction-program-scientific-main
+```
+
+2. Launch the program with the command
+```
+python main_program.py
+```
+
+# Example 2 (advanced)
+
+The program has various additional arguments that are listed here:
+
+- --input
+- --output
+- --optimize-length-scales-automatically
+- --length-scales
+- --input-values
+- --test-size
+- --seed
+
+1. [very useful] Input
+
+```
+python main_program.py --input Min` grad` angle`(rad/C`) --input Benecel`(%`)
+```
+
+Note: Special characters such as whitespaces and parentheses need to have the escape character '`' in front of them.
+
+You can add everything from 1 to 3 different inputs to the program. The predictions printed in the terminal as well as the figures in the "figures" -folder will all change depending on which input variables you choose to use for your prediction.
+
+2. [very useful] Output
+
+```
+python main_program.py --output Yield` stress`(Pa*10^6`)
+```
+
+You can only have 1 output variable when running the program. This is the variable being predicted.
+
+3. [technical] Optimize length scales automatically
+
+```
+python main_program.py --optimize-length-scales-automatically
+```
+
+This will turn on the built-in optimizer and determine length scales based on that. I don't recommend turning this setting on (it is off by default). It is however there as an option for those who want to try it out.
+
+If you're unsure, don't use this argument and stick with the default settings of the program.
+
+4. [technical] Length scales
+
+```
+python main_program.py --length-scales 10 --length-scales 0.1
+```
+
+The length scale values control the behaviour of the [RBF kernel](https://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.kernels.RBF.html) in the [GPR model](https://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.GaussianProcessRegressor.html). The length scale values have to always be larger than zero. Each length scale value corresponds to one input variable, and thus you should include either no arguments of this type (in which case they are inferred using a pretty good heuristic that is NOT the bad optimizer mentioned earlier), or alternatively the same number of arguments as the number of input variables.
+
+If you're unsure, don't use this argument and stick with the default settings of the program.
+
+5. [useful] Input values
+
+```
+python main_program.py --input-values -0.037 --input-values 761.21
+```
+
+What if you want to know the predicted value of one specific point? Even though the figures give a good general picture of what's going on, you might want to know the predicted value of one specific point. Naturally here as well you have to have the same number of arguments as the number of input variables.
+
+```
+---Specific point view---
+Input value of Min grad angle(rad/C): -0.037
+Input value of Storage modulus at min grad angle(Pa): 761.21
+Output value of Yield stress(Pa*10^6): 0.156
+-----------END-----------
+```
+
+Note: The printed text in your terminal might differ from this if the dataset used has been altered since the making of this guide.
+
+6. [technical] Test size
+
+```
+python main_program.py --test-size 0.4
+```
+
+You might want to alter the ratio to which the training and validation set size are split to. The default for this is 0.1 (training set is 90% and validation set is 10%), but a larger validation set size might sometimes be desired especially when more data is gathered over time.
+
+7. [technical] Seed
+
+```
+python main_program.py --seed 7
+```
+
+The seed argument is used to make the program deterministic. In short, by using the same seed you will always get the same result no matter how many times you run the program. The seed 0 is used by default.
+
+# Analysis
+
+The error of the GPR model is printed in the terminal:
+
+```
+---Model error---
+Relative training error: 35.249%
+Relative validation error: 37.295%
+-------END-------
+```
+
+PCA is also performed on the data. Figures can be found in the "figures" -folder.
+
+# How to add more data to the model
+
+## DMTA data (rheology)
+
+1. Add your data files to the "data" -folder
+
+2. Parse your data files in data_methods.py
+
+```
+data = [
+    pd.read_table('{}Rh_DMTA_000Storacell15Benecel_1Hz.txt'.format(path_1), decimal=',', comment='#', encoding='utf-16'),
+    pd.read_table('{}Rh_DMTA_025Storacell15Benecel_1Hz.txt'.format(path_1), decimal=',', comment='#', encoding='utf-16'),
+    # ...
+    # PARSE YOUR DATA FILES HERE
+]
+```
+
+## Compression test data
+
+1. Hard code the compression test data to data_methods.py
+
+```
+yield_stress = [
+    0.03973, 0.05190,
+    # ...
+    # ADD YOUR DATA HERE
+]
+yield_strain = [
+    6.540, 6.274,
+    # ...
+    # ADD YOUR DATA HERE
+]
+youngs_modulus = [
+    0.607, 1.098,
+    # ...
+    # ADD YOUR DATA HERE
+]
+
+conc_storacell = [
+    0.00, 0.25,
+    # ...
+    # ADD YOUR DATA HERE
+]
+conc_benecel = [
+    1.50, 1.50,
+    # ...
+    # ADD YOUR DATA HERE
+]
+lab_density = [
+    20.11, 24.70,
+    # ...
+    # ADD YOUR DATA HERE
+]
+```
+
+Note: Make sure that the new data is inserted in the same order in both the "data" -variable (DMTA data) as well as the "yield_stress", "yield_strain", ..., "lab_density" -variables (compression test data).
+
+Note: If you have lots of data (too much to hard code) you can of course also just add the compression test data files to the "data" -folder and then instead of hard coding the data parse the data.
+
+# How to add more variables to the model
+
+As in the "Compression test data" -subsection, you can complete step 1 either by hard coding individual numbers or by parsing data files. We will be parsing data files in this example.
+
+1. Add your data files to the "data" -folder
+
+2. Parse your data files in data_methods.py
+
+```
+yield_stress = [
+    0.03973, 0.05190,
+    # ...
+]
+yield_strain = [
+    6.540, 6.274,
+    # ...
+]
+# ...
+# ADD YOUR NEW VARIABLE HERE:
+# new_variable = parsing_program(path)
+```
+
+Note: Make sure the new variable has the same amount of elements as all the other data variables. Your new variable should for example have the same number of elements as the "data" -variable.
+
+3. Add the name of your variable to the "columns" -variable
+
+```
+columns = [
+    'Min grad angle(rad/C)',
+    'Storage modulus at min grad angle(Pa)',
+    # ...
+    # 'NAME OF NEW VARIABLE'
+]
+```
+
+4. Add your new data variable to the return value of the "load_data" -method
+
+```
+return [min_grad_angle, storage_modulus_at_min_grad_angle, ..., new_variable], columns
+```
---
+++ main_program.py
@@ -0,0 +1,404 @@
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel, RBF, WhiteKernel
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from scipy.interpolate import LinearNDInterpolator
+from iteration_utilities import deepflatten
+import argparse
+import additional_methods as add_methods
+import data_methods
+
+print('---Design the perfect FoamWood block suited for your needs using AI---\n')
+
+data, columns = data_methods.load_data()
+
+# Set signal and noise variance
+signal_variance_all = { x: 0.5 for x in columns }
+noise_variance_all = { x: 0 for x in columns }
+
+# Configure parser
+parser = argparse.ArgumentParser(description='Scientific GPR program')
+
+parser.add_argument(
+    '--input', action='append', choices=columns, default=None,
+    help="input variable(s) (default: ['Min grad angle(rad/C)', 'Storage modulus at min grad angle(Pa)'])"
+)
+parser.add_argument(
+    '--output', choices=columns, default='Yield stress(Pa*10^6)',
+    help="output variable (default: 'Yield stress(Pa*10^6)')"
+)
+parser.add_argument(
+    '--optimize-length-scales-automatically', action='store_false',
+    help='use the built-in optimizer to determine length scales (default: False) (warning: turning this feature on usually yields bad results)'
+)
+parser.add_argument(
+    '--length-scales', action='append', default=None, type=float,
+    help='length scale(s) of the input variable(s) (default: inferred)'
+)
+parser.add_argument(
+    '--input-values', action='append', default=None, type=float,
+    help='value(s) of the input variable(s) (default: inferred)'
+)
+parser.add_argument(
+    '--test-size', default=0.1, type=float,
+    help='validation set size (default: 0.1)'
+)
+parser.add_argument(
+    '--seed', default=0, type=int,
+    help='seed for random shuffling (default: 0)'
+)
+
+args = parser.parse_args()
+config = vars(args)
+
+input, output, fix_length_scales, test_size, seed = config['input'], config['output'], config['optimize_length_scales_automatically'], config['test_size'], config['seed']
+
+# Set default value for input arguments
+if input == None:
+    input = ['Min grad angle(rad/C)', 'Storage modulus at min grad angle(Pa)']
+
+# Sanitize arguments
+if len(input) > 3:
+    raise ValueError('number of input variables must be 3 or below')
+
+# Create DataFrame-objects
+X_data = pd.DataFrame({ x: y for x, y in zip(columns, data) })
+Y_data = pd.DataFrame({ x: y for x, y in zip(columns, data) })
+
+X_dropped_col = [ x for x in X_data.columns if x not in input ]
+Y_dropped_col = [ x for x in Y_data.columns if x not in output ]
+
+X_data = X_data.drop(columns=X_dropped_col)
+Y_data = Y_data.drop(columns=Y_dropped_col)
+
+# Infer length scales and input values
+# For further reading on why the length scale bounds are defined like this:
+# https://stats.stackexchange.com/questions/297673/how-to-pick-length-scale-bounds-for-rbc-kernels-in-gaussian-process-regression
+length_scale_min = [ add_methods.min_dist_arr(X_data[x]) for x in X_data.columns ]
+length_scale_max = [ add_methods.max_dist_arr(X_data[x]) for x in X_data.columns ]
+length_scales = [ round((x + y) / 2, 2) for x, y in zip(length_scale_max, length_scale_min) ]
+length_scale_bounds = [ (x, y) for x, y in zip(length_scale_min, length_scale_max) ]
+
+input_min = [ X_data[x].min() - ((X_data[x].max() - X_data[x].min()) / 10) for x in X_data.columns ]
+input_max = [ X_data[x].max() + ((X_data[x].max() - X_data[x].min()) / 10) for x in X_data.columns ]
+input_values = [ round((x + y) / 2, 2) for x, y in zip(input_max, input_min) ]
+
+# Override length scales and input values if manual arguments have been given
+if config['length_scales'] != None:
+    # Sanitize arguments
+    if len(config['length_scales']) != len(input):
+        raise ValueError('number of length scale arguments must match the number of input variables')
+
+    length_scales = config['length_scales']
+if config['input_values'] != None:
+    # Sanitize arguments
+    if len(config['input_values']) != len(input):
+        raise ValueError('number of input value arguments must match the number of input variables')
+
+    input_values = config['input_values']
+
+# Configure signal variance and noise variance
+signal_variance = signal_variance_all[Y_data.columns[0]]
+noise_variance = noise_variance_all[Y_data.columns[0]]
+
+# Initialize kernel and GP regressor
+if fix_length_scales:
+    length_scale_bounds = 'fixed'
+
+rbf = ConstantKernel(constant_value=signal_variance) * \
+RBF(length_scale=length_scales, length_scale_bounds=length_scale_bounds) + \
+WhiteKernel(noise_level=noise_variance)
+
+gpr = GaussianProcessRegressor(kernel=rbf, alpha=0.0)
+
+# Split the data
+X_train, X_val, Y_train, Y_val = add_methods.split_data(X_data, Y_data, test_size, seed)
+
+# Create GP model
+gpr.fit(X_train, Y_train)
+
+if len(X_data.columns) == 1:
+    fig = go.Figure()
+
+    # Create grid
+    x_val = np.linspace(
+        X_train[X_data.columns[0]].min() - ((X_train[X_data.columns[0]].max() - X_train[X_data.columns[0]].min()) / 10),
+        X_train[X_data.columns[0]].max() + ((X_train[X_data.columns[0]].max() - X_train[X_data.columns[0]].min()) / 10),
+        100
+    )
+    X_pred = x_val.reshape(-1, 1)
+
+    # Interpolate
+    # Color: https://waldyrious.net/viridis-palette-generator/
+    mu_val, std_val = add_methods.gp_pred(gpr, X_pred)
+
+    fig.add_trace(go.Scatter(
+        x=x_val, y=mu_val - std_val, line_width=0,
+        line_color='rgba(33, 145, 140, 1)', name=''
+    ))
+    fig.add_trace(go.Scatter(
+        x=x_val, y=mu_val + std_val, fill='tonexty', line_width=0,
+        line_color='rgba(33, 145, 140, 1)', name='Standard deviation'
+    ))
+    fig.add_trace(go.Scatter(
+        x=x_val, y=mu_val,
+        line_color='rgba(33, 145, 140, 1)', name='Interpolation function'
+    ))
+    fig.add_trace(go.Scatter(
+        x=X_train[X_train.columns[0]], y=Y_train[Y_train.columns[0]], customdata=X_train.index.tolist(),
+        line_color='rgba(16, 24, 32, 0.7)', mode='markers', name='Training data'
+    ))
+    fig.update_layout(
+        title='Interpolation of the training data set',
+        xaxis_title=X_data.columns[0],
+        yaxis_title=Y_data.columns[0]
+    )
+
+    # Calculate output value
+    X_input = pd.DataFrame({X_data.columns[0]: [input_values[0]]})
+    output_value, std_val = add_methods.gp_pred(gpr, X_input)
+    output_value = output_value[0]
+elif len(X_data.columns) == 2:
+    # Create grid
+    n = 100
+    x_val, y_val = [ np.linspace(
+        X_train[x].min() - ((X_train[x].max() - X_train[x].min()) / 10),
+        X_train[x].max() + ((X_train[x].max() - X_train[x].min()) / 10),
+        n
+    ) for x in X_data.columns ]
+    xx, yy = np.meshgrid(x_val, y_val)
+    X_pred = np.c_[xx.ravel(), yy.ravel()]
+
+    # Interpolate
+    mu_val, std_val = add_methods.gp_pred(gpr, X_pred)
+    mu_val = np.array(mu_val).reshape((n, n))
+    std_val = np.array(std_val).reshape((n, n))
+
+    fig = go.Figure(data=[
+        go.Surface(
+            z=mu_val, x=x_val, y=y_val, coloraxis='coloraxis', opacity=0.8, name='Interpolation function'),
+        go.Scatter3d(
+            x=X_train[X_train.columns[0]], y=X_train[X_train.columns[1]], z=Y_train[Y_train.columns[0]], customdata=X_train.index.tolist(),
+            mode='markers', marker={'size': 6, 'color': Y_train[Y_train.columns[0]], 'coloraxis': 'coloraxis'}, name='Training data')
+    ])
+    
+    fig.update_layout(
+        title='Interpolation of the training data set',
+        scene={
+            'xaxis_title': X_data.columns[0],
+            'yaxis_title': X_data.columns[1],
+            'zaxis_title': Y_data.columns[0],
+            'camera': {'eye': {'x': 2, 'y': 2, 'z': 2}}
+        },
+        coloraxis={'colorbar': {'title': {'text': '{}'.format(Y_data.columns[0])}}, 'colorscale': 'viridis'}
+    )
+
+    # Calculate output value
+    X_input = pd.DataFrame({ x: y for x, y in zip(X_data.columns, [[input_values[0]], [input_values[1]]]) })
+    output_value, std_val = add_methods.gp_pred(gpr, X_input)
+    output_value = output_value[0]
+elif len(X_data.columns) == 3:
+    # Create grid
+    n = 10
+    x_val, y_val, z_val = [ np.linspace(
+        X_train[x].min() - ((X_train[x].max() - X_train[x].min()) / 10),
+        X_train[x].max() + ((X_train[x].max() - X_train[x].min()) / 10),
+        n
+    ) for x in X_data.columns ]
+    xx, yy, zz = np.meshgrid(x_val, y_val, z_val)
+    X_pred = np.c_[xx.ravel(), yy.ravel(), zz.ravel()]
+
+    # Interpolate
+    mu_val, std_val = add_methods.gp_pred(gpr, X_pred)
+
+    fig = go.Figure(data=[
+        go.Scatter3d(
+            x=xx.ravel(), y=yy.ravel(), z=zz.ravel(), text=[ 'color: {}'.format(x) for x in mu_val ],
+            mode='markers', marker={'size': 4, 'color': mu_val, 'coloraxis': 'coloraxis'}, name='Interpolation function'),
+        go.Scatter3d(
+            x=X_train[X_train.columns[0]], y=X_train[X_train.columns[1]], z=X_train[X_train.columns[2]], text=[ 'color: {}'.format(x) for x in Y_train[Y_train.columns[0]] ], customdata=X_train.index.tolist(),
+            mode='markers', marker={'size': 12, 'color': Y_train[Y_train.columns[0]], 'coloraxis': 'coloraxis'}, name='Training data')
+    ])
+    
+    fig.update_layout(
+        title='Interpolation of the training data set',
+        scene={
+            'xaxis_title': X_data.columns[0],
+            'yaxis_title': X_data.columns[1],
+            'zaxis_title': X_data.columns[2],
+            'camera': {'eye': {'x': 2, 'y': 2, 'z': 2}}
+        },
+        coloraxis={'colorbar': {'title': {'text': '{}'.format(Y_data.columns[0])}}, 'colorscale': 'viridis'},
+        showlegend=False
+    )
+
+    # Calculate output value
+    X_input = pd.DataFrame({ x: y for x, y in zip(X_data.columns, [[input_values[0]], [input_values[1]], [input_values[2]]]) })
+    output_value, std_val = add_methods.gp_pred(gpr, X_input)
+    output_value = output_value[0]
+
+# Return output graph
+fig.write_html('figures/general_view.html')
+
+# Return input and output values
+print('---Specific point view---')
+print(*[ 'Input value of {}: {}'.format(x, round(y, 3)) for x, y in zip(X_data.columns, input_values) ], sep='\n')
+print('Output value of {}: {}'.format(Y_data.columns[0], round(output_value, 3)))
+print('-----------END-----------\n')
+
+# Model error
+mu_val_train, std_val_train = add_methods.gp_pred(gpr, X_train)
+mu_val_val, std_val_val = add_methods.gp_pred(gpr, X_val)
+
+train_error, val_error = add_methods.gp_error(mu_val_train, mu_val_val, std_val_val, X_val, Y_train, Y_val)
+
+# Return model error
+print('---Model error---')
+print('Relative training error: {}%'.format(round(train_error, 3)))
+print('Relative validation error: {}%'.format(round(val_error, 3)))
+print('-------END-------\n')
+
+# Draw PCA figure
+X_data_pca = pd.DataFrame({ x: y for x, y in zip(columns, data) })
+X_data_pca = X_data_pca.drop(columns=Y_data.columns[0])
+Y_data_pca = pd.DataFrame({ x: y for x, y in zip(columns, data) })
+Y_data_pca = Y_data_pca.drop(columns=[ x for x in columns if not x == Y_data.columns[0] ])
+
+X_data_pca[X_data_pca.columns] = StandardScaler().fit_transform(X_data_pca[X_data_pca.columns])
+
+# PCA with 2 principal components
+pca = PCA(n_components=2)
+pca.fit(X_data_pca)
+X_data_pca_2pc = pca.transform(X_data_pca)
+
+# Create grid
+n = [150, 50]
+x_val, y_val = [ np.linspace(
+    X_data_pca_2pc[:, x].min() - ((X_data_pca_2pc[:, x].max() - X_data_pca_2pc[:, x].min()) / 10),
+    X_data_pca_2pc[:, x].max() + ((X_data_pca_2pc[:, x].max() - X_data_pca_2pc[:, x].min()) / 10),
+    y
+) for x, y in zip(range(0, 2), n) ]
+xx, yy = np.meshgrid(x_val, y_val)
+
+# Interpolate
+interp = LinearNDInterpolator(X_data_pca_2pc, Y_data_pca[Y_data_pca.columns[0]])
+col_interp = interp(xx, yy)
+xx = [ [ b for a, b in zip(x, y) if not np.isnan(a) ] for x, y in zip(col_interp, xx) ]
+yy = [ [ b for a, b in zip(x, y) if not np.isnan(a) ] for x, y in zip(col_interp, yy) ]
+color_interp = [ [ y for y in x if not np.isnan(y) ] for x in col_interp ]
+xx = list(deepflatten(xx))
+yy = list(deepflatten(yy))
+color_interp = list(deepflatten(color_interp))
+
+fig_pca_2pc = go.Figure()
+
+fig_pca_2pc.add_trace(go.Scatter(
+    x=xx, y=yy, text=[ 'color: {}'.format(x) for x in color_interp ],
+    mode='markers', marker={'size': 4, 'color': color_interp, 'coloraxis': 'coloraxis'}, name='Linear interpolation'
+))
+fig_pca_2pc.add_trace(go.Scatter(
+    x=X_data_pca_2pc[:, 0], y=X_data_pca_2pc[:, 1], text=[ 'color: {}'.format(x) for x in Y_data_pca[Y_data_pca.columns[0]] ],
+    mode='markers', marker={'size': 12, 'color': Y_data_pca[Y_data_pca.columns[0]], 'coloraxis': 'coloraxis'}, name='Training data'
+))
+fig_pca_2pc.update_layout(
+    title='PCA with 2 principal components',
+    xaxis_title='PC1',
+    yaxis_title='PC2',
+    xaxis_range=[x_val[0], x_val[-1]],
+    yaxis_range=[y_val[0], y_val[-1]],
+    coloraxis={'colorbar': {'title': {'text': '{}'.format(Y_data.columns[0])}}, 'colorscale': 'viridis'},
+    showlegend=False
+)
+
+# Return PCA with 2PC graph
+fig_pca_2pc.write_html('figures/pca_2pc.html')
+
+# PCA with 3 principal components
+pca = PCA(n_components=3)
+pca.fit(X_data_pca)
+X_data_pca_3pc = pca.transform(X_data_pca)
+
+# Create grid
+n = 10
+x_val, y_val, z_val = [ np.linspace(
+    X_data_pca_3pc[:, x].min() - ((X_data_pca_3pc[:, x].max() - X_data_pca_3pc[:, x].min()) / 10),
+    X_data_pca_3pc[:, x].max() + ((X_data_pca_3pc[:, x].max() - X_data_pca_3pc[:, x].min()) / 10),
+    n
+) for x in range(0, 3) ]
+xx, yy, zz = np.meshgrid(x_val, y_val, z_val)
+
+# Interpolate
+interp = LinearNDInterpolator(X_data_pca_3pc, Y_data_pca[Y_data_pca.columns[0]])
+col_interp = interp(xx, yy, zz)
+xx = [ [ [ j for i, j in zip(a, b) if not np.isnan(i) ] for a, b in zip(x, y) ] for x, y in zip(col_interp, xx) ]
+yy = [ [ [ j for i, j in zip(a, b) if not np.isnan(i) ] for a, b in zip(x, y) ] for x, y in zip(col_interp, yy) ]
+zz = [ [ [ j for i, j in zip(a, b) if not np.isnan(i) ] for a, b in zip(x, y) ] for x, y in zip(col_interp, zz) ]
+color_interp = [ [ [ i for i in a if not np.isnan(i) ] for a in x ] for x in col_interp ]
+xx = list(deepflatten(xx))
+yy = list(deepflatten(yy))
+zz = list(deepflatten(zz))
+color_interp = list(deepflatten(color_interp))
+
+fig_pca_3pc = go.Figure(data=[
+    go.Scatter3d(
+        x=xx, y=yy, z=zz, text=[ 'color: {}'.format(x) for x in color_interp ],
+        mode='markers', marker={'size': 4, 'color': color_interp, 'coloraxis': 'coloraxis'}, name='Linear interpolation'),
+    go.Scatter3d(
+        x=X_data_pca_3pc[:, 0], y=X_data_pca_3pc[:, 1], z=X_data_pca_3pc[:, 2], text=[ 'color: {}'.format(x) for x in Y_data_pca[Y_data_pca.columns[0]] ],
+        mode='markers', marker={'size': 12, 'color': Y_data_pca[Y_data_pca.columns[0]], 'coloraxis': 'coloraxis'}, name='Training data')
+])
+
+fig_pca_3pc.update_layout(
+    title='PCA with 3 principal components',
+    scene={
+        'xaxis_title': 'PC1',
+        'yaxis_title': 'PC2',
+        'zaxis_title': 'PC3',
+        'xaxis_range': [x_val[0], x_val[-1]],
+        'yaxis_range': [y_val[0], y_val[-1]]
+    },
+    coloraxis={'colorbar': {'title': {'text': '{}'.format(Y_data.columns[0])}}, 'colorscale': 'viridis'},
+    showlegend=False
+)
+
+# Return PCA with 3PC graph
+fig_pca_3pc.write_html('figures/pca_3pc.html')
+
+# PCA table
+pca_table_columns = ['Feature', 'Component 1', 'Component 2', 'Component 3']
+comp = pca.components_
+pca_table_data = [ {
+    pca_table_columns[0]: col,
+    pca_table_columns[1]: round(pc1, 3),
+    pca_table_columns[2]: round(pc2, 3),
+    pca_table_columns[3]: round(pc3, 3)
+} for col, pc1, pc2, pc3 in zip(X_data_pca.columns, comp[0, :], comp[1, :], comp[2, :]) ]
+
+# Return PCA table
+pd.DataFrame(pca_table_data).to_csv('output_tables/pca_table.csv')
+
+# Draw parallel coordinates figure
+fig_par_coo = go.Figure(data=
+    go.Parcoords(
+        line={
+            'color': Y_train[Y_train.columns[0]],
+            'colorscale': 'viridis',
+            'showscale': True
+        },
+        dimensions=[ {'label': x, 'values': X_train[x]} for x in X_train.columns ] + [{'label': Y_train.columns[0], 'values': Y_train[Y_train.columns[0]]}]
+    )
+)
+
+# Return parallel coordinates figure
+fig_par_coo.write_html('figures/par_coo.html')
+
+# Data set table
+data_table_columns = ['Data point'] + columns
+XY_data = pd.DataFrame({ x: y for x, y in zip(columns, data) })
+data_table_data = [ dict(zip(data_table_columns, [col] + [ round(x, 3) for x in data ])) for col, data in zip(range(0, len(XY_data.index)), zip(*np.transpose(XY_data.to_numpy()).tolist())) ]
+
+# Return data table
+pd.DataFrame(data_table_data).to_csv('output_tables/data_table.csv')
